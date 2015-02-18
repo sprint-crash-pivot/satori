01fa3f2bf2e81f393ce84057cf7500ff5cc7c910
Corey Docken <corey@dockenonline.com>
2015-02-18 14:55:45 -0600

    added rails module

--- lib/satori.rb
+++ lib/satori.rb
@@ -1,5 +1,8 @@
 require "satori/version"
 
 module Satori
-  # Your code goes here...
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
 end
--- lib/satori/version.rb
+++ lib/satori/version.rb
@@ -1,3 +1,3 @@
 module Satori
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
