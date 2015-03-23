9abc9240680a71722ad141741de2650e4d8f4ca1
Corey Docken <corey@dockenonline.com>
2015-03-23 14:26:08 -0500

    added a content modal window

--- vendor/assets/javascripts/satori.js
+++ vendor/assets/javascripts/satori.js
@@ -15,7 +15,7 @@ var CopyContents = {
 		var $this = $(this),
 			$source = $($this.data("source-selector")),
 			$target = $($this.data("target-selector"));
-		
+
 		if ($source.length == 0) {
 			// Logger.log("CopyContents Error: data-source-selector could not find an element..");
 			return false;
@@ -23,10 +23,10 @@ var CopyContents = {
 			// Logger.log("CopyContents Error: data-target-selector could not find an element..");
 			return false;
 		}
-		
+
 		// TODO don't copy hidden rails elements
 		var $clone = $source.children().clone().appendTo($target);
-		
+
 		$clone.find("[name]").attr("name", $.proxy(CopyContents._changeNameCount, null, $target));
 	},
 	_changeNameCount: function($target) {
@@ -49,13 +49,13 @@ var ToggleMenu = {
 	ready: function(event) {
 		var $menu = $("[data-role*=" + ToggleMenu._dataRole + "]"),
 			$target = $($menu.data("target"));
-		
+
 		if ($target.length) {
 			ToggleMenu._window = $(window);
 			ToggleMenu._document = $(document);
 			ToggleMenu._menu = $menu;
 			ToggleMenu._target = $target;
-			
+
 			$menu.on("click." + ToggleMenu._clickEvent, ToggleMenu.click);
 			$target.addClass("toggle-menu");
 		}
@@ -63,7 +63,7 @@ var ToggleMenu = {
 	click: function(event) {
 		ToggleMenu._window.on("click." + ToggleMenu._hideMenuEvent, ToggleMenu.docHideMenu);
 		ToggleMenu._target.show();
-		
+
 		event.stopPropagation();
 		event.preventDefault();
 	},
@@ -80,12 +80,12 @@ var ToggleEdit = {
 		$(document).on("click.toggleEditClick", "[data-role*=toggle-edit], [data-role*=toggle-save]", function(event) {
 			event.stopPropagation();
 			event.preventDefault();
-			
+
 			var $this = $(this),
 				$editable = $this.closest("[data-role*=editable]"),
 				$show = $editable.find(".show"),
 				$edit = $editable.find(".edit");
-				
+
 			if ($show.is(":visible")) {
 				$show.hide();
 				$edit.show();
@@ -97,11 +97,99 @@ var ToggleEdit = {
 	}
 };
 
+ModalWindow = {
+	ready: function(event, $document) {
+		console.log("ModalWindow");
+
+		var $overlay = ModalWindow.findOverlay();
+		var $modal = ModalWindow.findModal();
+
+		$document.on("click.open-modal", "[data-role*=modal]", ModalWindow.open);
+	},
+	open: function(event) {
+		var $link = $(event.target),
+				$overlay = ModalWindow.findOverlay(),
+				$modal = ModalWindow.findModal();
+
+		var href = $link.data("href"),
+				$container = $($link.data("container"));
+
+		if (href) {
+			// show loading screen
+			// make a promise, ajax call
+			// ajax call contents to modal
+			// hide loading screen
+		} else if ($container.length) {
+			$modal.find("#satori-modal-title").text($link.data("title"));
+			$modal.find("#satori-modal-content").html($container.html());
+		} else {
+			Logger.log("Warning: No modal window content found.");
+		}
+
+		$overlay.show();
+		$modal.show();
+
+		ModalWindow.resize($modal);
+	},
+	close: function(event) {
+		var $overlay = ModalWindow.findOverlay(),
+				$modal = ModalWindow.findModal();
+
+		$overlay.hide();
+		$modal.hide();
+	},
+	resize: function ($modal) {
+		var $window = $(window);
+
+		console.log($window.scrollTop());
+		console.log($window.height());
+
+		$modal.css({
+			top: ($window.scrollTop() + ($window.height() / 10)),
+			left: $window.width() / 10
+		});
+	},
+	findOverlay: function() {
+		var $overlay = $("#satori-overlay");
+
+		if ($overlay.length === 0) {
+			$overlay = $("<div id=\"satori-overlay\">");
+
+			$overlay.appendTo(document.body);
+			$overlay.on("click.close-modal", ModalWindow.close);
+		}
+
+		return $overlay;
+	},
+	findModal: function() {
+		var $modal = $("#satori-modal");
+
+		if ($modal.length === 0) {
+			$modal = $("<div id=\"satori-modal\">");
+
+			var $toolbar = $("<header id=\"satori-modal-top-toolbar\" class=\"bg-primary\"><div id=\"satori-modal-title\"></div></header>"),
+					$btnClose = $("<div id=\"satori-modal-close\" class=\"right fake-link mobile-icon close no-text no-margin\">Close</div>"),
+					$content = $("<div id=\"satori-modal-content\">");
+
+			$btnClose.on("click.close-modal", ModalWindow.close).prependTo($toolbar);
+
+			$toolbar.appendTo($modal);
+			$content.appendTo($modal);
+
+			$modal.appendTo(document.body);
+		}
+
+		return $modal;
+	}
+};
+
 var $document = $(document);
 
 $document.on("page:load ready", function(event) {
 	console.log("Satori!");
-	
+
+	ModalWindow.ready(event, $document);
+
 	CopyContents.ready(event);
 	ToggleMenu.ready(event);
 	ToggleEdit.ready(event);
@@ -116,11 +204,11 @@ $document.on("page:load ready", function(event) {
             $closest.find("[name*='_destroy'][value='false']").val("1");
             event.preventDefault();
         }
-        
+
         // there's a bug here that binds the event twice
         event.stopPropagation();
     });
-	
+
 	// start toggle
 	$document.on("change.toggle-object", "[data-role*=select-tog]", function(event) {
 		var $select = $(this),
@@ -129,14 +217,14 @@ $document.on("page:load ready", function(event) {
 			container = $select.data("toggle-container") || document,
 			$container = $(container),
 			$names = $container.find("[data-toggle-name*=" + selector + "]");
-		
+
 		$names.find("[data-toggle-value]").each(function(i, obj) {
 			var $obj = $(obj),
 				value = $obj.data("toggle-value");
 
 			if (obj !== undefined) {
 				values = value.split(",");
-				
+
 				if ($.inArray(selectorValue, values) != -1) {
 					$obj.show();
 				} else {
